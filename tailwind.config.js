c979e22757dcf4ffd623a953c67d93a513855e0c
Michelle Mounde <mitchmounde@gmail.com>
2023-07-24 13:12:27 +0300

    refactor: install and setup tailwind to replace plain css

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: {
+        light: 'ffffffde',
+        DEFAULT: 'ffffff',
+        dark: 'f9f9f9'
+      },
+      purple: {
+        light: 'd0d6f9',
+        default: '747bff',
+        dark: '535bf2',
+        darkest: '646cff',
+      },
+      black: {
+        light: '242424',
+        DEFAULT: '1a1a1a',
+        dark: '0b0d17'
+      },
+      midnight: {
+        light: '213547'
+      }
+    },
+    textColor: 'ffffffde',
+    extend: {},
+  },
+  plugins: [],
+}
