989cdb07c6902007f196bdcf67700d617125bed5
Michelle Mounde <mitchmounde@gmail.com>
2023-07-31 20:05:21 +0300

    refactor: finish transforming CSS to Tailwind but with custom classes not working error

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    textColor: "ffffffde",
+    fontFamily: {
+      sans: ["Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"]
+    },
     colors: {
       white: {
-        light: 'ffffffde',
-        DEFAULT: 'ffffff',
-        dark: 'f9f9f9'
+        light: "ffffffde",
+        DEFAULT: "ffffff",
+        dark: "f9f9f9"
       },
       purple: {
-        light: 'd0d6f9',
-        default: '747bff',
-        dark: '535bf2',
-        darkest: '646cff',
+        light: "d0d6f9",
+        default: "747bff",
+        dark: "535bf2",
+        darkest: "646cff",
       },
       black: {
-        light: '242424',
-        DEFAULT: '1a1a1a',
-        dark: '0b0d17'
+        light: "242424",
+        DEFAULT: "1a1a1a",
+        dark: "0b0d17"
       },
       midnight: {
         light: '213547'
       }
     },
-    textColor: 'ffffffde',
-    extend: {},
+    extend: {
+      fontFamily: {
+        inherit: ['inherit'],
+      }
+    },
   },
   plugins: [],
 }
